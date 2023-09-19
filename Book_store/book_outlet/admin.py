deb4327af9ca2874f2371626da9904b0fb7a8115
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-19 11:39:12 +0200

    More Config Options

--- Book_store/book_outlet/admin.py
+++ Book_store/book_outlet/admin.py
@@ -4,5 +4,7 @@ from .models import *
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating")
+    list_display = ("title", "author")
 
 admin.site.register(Book, BookAdmin)
