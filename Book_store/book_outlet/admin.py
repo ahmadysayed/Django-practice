2daa9ed09541948a1389965b53fe92d9b6978af1
ahmadysayed <ahmadysayedmujtaba@gmail.com>
2023-09-19 11:31:39 +0200

    Configuring the Admin Settings

--- Book_store/book_outlet/admin.py
+++ Book_store/book_outlet/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
 
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
